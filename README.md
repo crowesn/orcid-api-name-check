af0670ef49a3871185edc55bb0568614dcc9f8a4
Katie <katie@flutterflies.net>
2017-10-09 16:39:07 -0400

    Refactor script to python :snake:

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # ORCID api name check
 
 Script to query ORCID public api for names affiliated with UC
+
+## Usage (Python)
+#### Requirements
+* Python version 3.6+
+* Pip for Python 3
+
+#### Setup
+* Install dependencies using `pip3 install -r "requirements.txt"`
+* Add a `names.csv` to the directory with the names you want to query in ORCID
+* Edit `check_names.py` and add your ORCID API token
+
+#### Running 
+* Run with `python3 check_names.py`
+
+## Usage (Ruby)
+#### Requirements
+* Ruby version 2.4+
+ 
+#### Setup
+* Install namae gem using `gem install namae`
+* Add a `names.csv` to the directory with the names you want to query in ORCID
+* Edit `check_names.rb` and add your ORCID API token
+
+#### Running 
+* Run with `ruby check_names.rb`
---
+++ check_names.py
@@ -0,0 +1,47 @@
+from nameparser import HumanName
+from urllib import parse
+import requests
+import csv
+import json
+
+token = '' # Your API Token Here
+base_url = 'https://pub.sandbox.orcid.org/v2.0/search/'
+org_name = 'cincinnati'
+csv_file = 'names.csv'
+output_file = 'results.csv'
+results = []
+
+def parse_csv():
+  with open(csv_file, newline = '') as csvfile:
+    name_reader = csv.reader(csvfile, delimiter = ';')
+    for row in name_reader:
+      query_results = query_api(parse_name(row[0]))['result']
+      if query_results:
+        for query_result in query_results:
+          results.append(f"{row[0]}, {query_result['orcid-identifier']['uri']}\n")
+      else:
+        results.append(f"{row[0]}, --\n")
+
+def parse_name(input_name):
+  name = HumanName(input_name)
+  return name
+
+def query_api(name):
+  url = (f"{base_url}?q=given-names:{parse.quote_plus(name.first)}~+AND+family-name:"
+         f"{parse.quote_plus(name.last)}&fq=affiliation-org-name:{org_name}")
+
+  head = { 'Authorization type':'Bearer',
+          'Content-Type':'application/json',
+          'Access token':token }
+
+  response = requests.get(url, headers = head)
+  return response.json()
+
+def write_csv():
+  output = open(output_file, 'w')
+  for result in results:
+    output.write(result)
+  output.close
+
+parse_csv()
+write_csv()
